c710a49154a826fbcac294111440d37d7e282dcb
DraconDev <dracsharp@gmail.com>
2023-11-10 04:13:35 +0000

    add test endpoint receives requests

--- app/api/testing/route.tsx
+++ app/api/testing/route.tsx
@@ -1,10 +1,12 @@
-export async function POST() {
+export async function POST(request: Request) {
+    const test = await request.json();
     const res = {
         method: "POST",
         headers: {},
         body: JSON.stringify({ time: new Date().toISOString() }),
     };
 
+    console.log(test);
     const data = await res;
 
     return Response.json(data);
