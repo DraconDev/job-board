67fbf9fe04a3235d9cd5f0938252796ad9f449d9
DraconDev <dracsharp@gmail.com>
2023-11-16 12:28:17 +0000

    add save job to db

--- app/api/jobs/route.tsx
+++ app/api/jobs/route.tsx
@@ -1,10 +1,30 @@
+import { addJob, checkDB } from "@/db/mongo";
+import { Job } from "@/type/types";
 
 export async function POST(request: Request) {
+    checkDB();
     const data = await request.json();
-    const { title, description, location, salaryMin, salaryMax, date } =
-        await data;
+    const {
+        title,
+        description,
+        location,
+        salaryMin,
+        salaryMax,
+        date,
+        jobType,
+        company,
+    } = (await data) as Job;
 
-
+    addJob({
+        title,
+        description,
+        location,
+        salaryMin,
+        salaryMax,
+        date,
+        jobType,
+        company,
+    });
 
     return new Response("Job saved", {});
 }
--- db/mongo.ts
+++ db/mongo.ts
@@ -1,39 +1,46 @@
 import { Job } from "@/type/types";
+import mongoose from "mongoose";
 import { JobPost } from "./schema";
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.cc5lbfe.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-export const mongoClient = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-});
+// export const mongoClient = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     },
+// });
 
-export async function checkDB() {
-    try {
-        // Connect the mongoClient to the server	(optional starting in v4.7)
-        await mongoClient.connect();
-        // Send a ping to confirm a successful connection
-        await mongoClient.db("admin").command({ ping: 1 });
-        console.log(
-            "Pinged your deployment. You successfully connected to MongoDB!"
-        );
-    } finally {
-        // Ensures that the mongoClient will close when you finish/error
-        await mongoClient.close();
-    }
+// export async function checkDB() {
+//     try {
+//         await mongoClient.connect();
+//         await mongoClient.db("admin").command({ ping: 1 });
+//         console.log(
+//             "Pinged your deployment. You successfully connected to MongoDB!"
+//         );
+//     } finally {
+//         await mongoClient.close();
+//     }
+// }
+
+async function connectDb() {
+    await mongoose.connect(uri);
 }
 
 export async function addJob(job: Job) {
+    console.log(job);
+    await connectDb();
     try {
         // Connect the mongoClient to the server	(optional starting in v4.7)
-        await mongoClient.connect();
-        const jobPost = new JobPost(job);
-        await jobPost.save();
+
+        // const jobPost = new JobPost(job);
+
+        // save the job
+        await JobPost.create(job);
+        // await jobPost.save();
+
         // Send a ping to confirm a successful connection
     } finally {
         // Ensures that the mongoClient will close when you finish/error
