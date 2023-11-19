3178c1ad7312d138ee4ae5ab51be61c8169a59c1
DraconDev <dracsharp@gmail.com>
2023-11-19 13:56:16 +0000

    del test code

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,7 @@ import JobContainer from "@/components/JobContainer";
 export default function Home() {
     return (
         <main className="gap-5 bg-gray-400 w-full">
-            {/* <button onClick={() => console.log("hello")}>testing</button> */}
             <JobContainer />
-            {/* <button onClick={() => checkDB().catch(console.dir)}>
-                testing
-            </button>
-            <div className=""></div>
-            <Link href="/joblist">joblist</Link>
-            <div className=""></div>
-            <Link href="/API/mozi" onClick={() => console.log("hello")}>
-                get hello
-            </Link> */}
         </main>
     );
 }
