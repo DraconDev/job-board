ee6c416d8d0d89aba50f5df4f3c005207882d93a
DraconDev <dracsharp@gmail.com>
2023-11-27 18:53:30 +0000

    fix jobcontainer load

--- components/JobContainer.tsx
+++ components/JobContainer.tsx
@@ -1,16 +1,21 @@
 "use client";
 import { useAppState } from "@/state/state";
+import { useEffect, useState } from "react";
 import Filter from "./Filter";
 import JobPanel from "./JobPanel";
 import NoJobs from "./NoJobs";
+
 export default function JobContainer() {
     const state = useAppState((state) => state);
+    const [isClient, setIsClient] = useState(false);
 
-    if (state.jobList.length === 0) {
-        return <NoJobs />;
-    }
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
 
-    return (
+    return !isClient || !state.jobList ? (
+        <NoJobs />
+    ) : (
         <div className="flex flex-col w-full h-full gap-2">
             <Filter />
             <JobPanel />
