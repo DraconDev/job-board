f7a743ee4a848ebd4b58b2a4e75c4771671a1f1d
DraconDev <dracsharp@gmail.com>
2023-11-30 16:45:05 +0000

    style navbar

--- components/LogoButton.tsx
+++ components/LogoButton.tsx
@@ -6,15 +6,17 @@ export default function Logo({ type }: { type: string }) {
         <>
             <Link href={type === "home" ? "/" : `/${type}`}>
                 {/* <Link href="work"> */}
-                <div className="flex items-center justify-center w-8 h-8 ">
-                    <Image
-                        src={`${type}.svg`}
-                        alt={type}
-                        width={30}
-                        height={30}
-                        title={type}
-                        className="invert"
-                    />
+                <div className="p-2 hover:bg-accent rounded-lg">
+                    <div className="flex items-center justify-center w-7 h-7 ">
+                        <Image
+                            src={`${type}.svg`}
+                            alt={type}
+                            width={30}
+                            height={30}
+                            title={type}
+                            className="invert "
+                        />
+                    </div>
                 </div>
             </Link>
         </>
