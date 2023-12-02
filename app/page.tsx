d7768d36f5cbc079f6d881045c3894f331e462e7
DraconDev <dracsharp@gmail.com>
2023-12-02 18:36:14 +0000

    fix ui jobcontainer

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import JobContainer from "@/components/JobContainer";
 
 export default function Home() {
     return (
-        <main className="gap-5 bg-gray-700 w-full overflow-hidden">
+        <main className="gap-5 bg-primary w-full overflow-hidden">
             <JobContainer />
         </main>
     );
--- components/Description.tsx
+++ components/Description.tsx
@@ -6,17 +6,25 @@ export default function Description() {
     const state = useAppState((state) => state);
 
     return (
-        <div className="flex  p-1 bg-primary flex-col h-full gap-1 overflow-auto h-[90vh]">
-            <div className="flex rounded p-2 shadow-md bg-secondary w-full gap-2 flex-col min-h-[400px] hover:transform hover:scale-[1.01] duration-150">
+        <div className="flex  p-1 bg-primary flex-col gap-1  h-[90vh] overflow-hidden">
+            <div className="flex rounded p-2 shadow-md bg-secondary w-full gap-2 flex-col min-h-[400px]  hover:transform hover:scale-[1.01] duration-150">
                 <div className="text-xl rounded font-semibold">
                     {state?.activeJobPost?.title}
                 </div>
                 <div className="w-full h-[2px] bg-blue-600"></div>
                 <div className="">{state?.activeJobPost?.description}</div>
             </div>
+
             <div className="flex gap-1">
                 <CustomButton text="Save" />
                 <CustomButton text="Apply" />
+            </div>
+            <div className=" w-full h-full bg-primary">
+                <div className="overflow-hidden">
+                    <div className="hover:scale-125 transition-transform bg-red-700 h-full w-full">
+                        Content inside the div that will scale on hover.
+                    </div>
+                </div>
             </div>
         </div>
     );
