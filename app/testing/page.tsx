eee59b1f775aa0259668eca4e023a32e3318b3a7
DraconDev <dracsharp@gmail.com>
2023-12-03 17:58:33 +0000

    test credentials

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 import { useAppState } from "@/state/state";
+import { useSession } from "next-auth/react";
 
 type Props = {};
 
 const Testing = (props: Props) => {
     const state = useAppState((state) => state);
+    const session = useSession();
 
     return (
         <div className="flex flex-col  w-full h-full    ">
+            <div className="">Account</div>
+            {JSON.stringify(session)}
             {/* <LoginLogo /> */}
             {/* <NoJobs /> */}
 
