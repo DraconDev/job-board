83c020b04191fcfe4f2f6f3e6d7338cb1bfafdf9
DraconDev <dracsharp@gmail.com>
2023-12-03 15:18:02 +0000

    del needless dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
             },
             colors: {
-                primary: "#191919", // black
+                primary: "#222", // black
                 secondary: "#333", // dark gray
                 accent: "#247C", // medium gray
             },
