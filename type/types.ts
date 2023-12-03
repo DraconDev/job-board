51cf6758e43eba0313734837c1aab09814c9501d
DraconDev <dracsharp@gmail.com>
2023-12-03 16:51:14 +0000

    del unused types

--- type/types.ts
+++ type/types.ts
@@ -1,18 +1,5 @@
-// import { Document } from "mongoose";
-
 import { userSchema } from "@/db/schema";
 import mongoose from "mongoose";
-
-// export interface Job extends Document {
-//     title: string;
-//     description: string;
-//     salaryMin: number;
-//     salaryMax: number;
-//     location: string;
-//     date: Date;
-//     jobType: string;
-//     company: string;
-// }
 
 export interface Job {
     title: string;
@@ -25,20 +12,5 @@ export interface Job {
     company: string;
     experience: string;
 }
-
-// export interface IUser {
-//     fullName: string;
-//     email: string;
-//     phoneNumber: string;
-//     education: { degree: string; institution: string; endDate: Date }[];
-//     skills: string[];
-//     workExperiences: {
-//         position: string;
-//         company: string;
-//         description: string;
-//         date: string;
-//     }[];
-//     jobsApplied: Job[]; // Assuming jobSchema is defined elsewhere
-// }
 
 export type UserType = mongoose.InferSchemaType<typeof userSchema>;
