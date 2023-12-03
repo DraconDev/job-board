9890b1c098a6715e5458d87ea225b22c5827bff8
DraconDev <dracsharp@gmail.com>
2023-12-03 17:17:21 +0000

    fix register

--- app/api/register/route.tsx
+++ app/api/register/route.tsx
@@ -5,5 +5,4 @@ import { registerUser } from "@/db/mongo";
 export async function POST(req: Request) {
     const data = await req.json();
     registerUser(data);
-    return Response.json({ success: true });
 }
