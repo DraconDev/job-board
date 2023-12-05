621e113bf5772a2b912acb2b294a0579e0078488
DraconDev <dracsharp@gmail.com>
2023-12-05 21:23:44 +0000

    style joblist

--- components/JobList.tsx
+++ components/JobList.tsx
@@ -5,7 +5,7 @@ import JobCard from "./JobCard";
 export default function JobList() {
     const state = useAppState((state) => state);
     return (
-        <div className="flex  p-2 bg-primary flex-col  gap-2 overflow-auto h-[90vh]">
+        <div className="flex  p-1 bg-primary flex-col  gap-2 overflow-auto h-[90vh]">
             {state.jobList.map((job, index) => (
                 <JobCard
                     job={job}
