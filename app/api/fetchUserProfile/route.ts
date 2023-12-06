80902808b7b5c15d38b3a280ec4bb28b93e9d7e9
DraconDev <dracsharp@gmail.com>
2023-12-06 20:38:32 +0000

    add fetchuserprofile

--- app/api/fetchUserProfile/route.ts
+++ app/api/fetchUserProfile/route.ts
@@ -1,7 +1,30 @@
-export async function GET() {
+import { uri } from "@/db/mongo";
+import { User } from "@/db/schema";
+import mongoose from "mongoose";
+
+async function fetchUser({ email }: { email: string }) {
+    await mongoose.connect(uri);
+    try {
+        const user = await User.findOne({ email });
+        return user;
+    } finally {
+        await mongoose.disconnect();
+    }
+}
+
+export async function GET(req: Request) {
+    const email = req.headers.get("email");
+    if (email === null || email.length < 6) {
+        throw new Error("Missing email in the request");
+    }
+    const user = await fetchUser({ email });
     // const jobs = await fetchRecentJobs();
     // const jobs = await fetchJobsByFilter({});
 
+    if (!user) {
+        throw new Error("User not found");
+    }
+
     // * return jobs
-    return new Response(JSON.stringify(jobs));
+    return new Response(JSON.stringify(user));
 }
