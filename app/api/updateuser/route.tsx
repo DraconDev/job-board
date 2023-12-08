948e43658d5a670d6e884fbb306a2091805023b9
DraconDev <dracsharp@gmail.com>
2023-12-08 22:49:11 +0000

    add updateuserprofile route

--- app/api/updateuser/route.tsx
+++ app/api/updateuser/route.tsx
@@ -1,8 +1,8 @@
-// * register user with cv
+// // * register user with cv
 
-import { updateUser } from "@/db/mongo";
+// import { updateUser } from "@/db/mongo";
 
-export async function POST(req: Request) {
-    const data = await req.json();
-    updateUser(data);
-}
+// export async function POST(req: Request) {
+//     const data = await req.json();
+//     updateUser(data);
+// }
--- app/api/updateuserprofile/route.tsx
+++ app/api/updateuserprofile/route.tsx
@@ -1,21 +1,8 @@
-// * register user with cv
 import { uri } from "@/db/mongo";
 import { User } from "@/db/schema";
 import { UserType } from "@/type/types";
 import mongoose from "mongoose";
 
-// * register user with CV
-async function registerUser(user: UserType) {
-    console.log(user);
-    await mongoose.connect(uri);
-    try {
-        await User.create(user);
-    } finally {
-        await mongoose.disconnect();
-    }
-}
-
-// * update user profile or create it
 async function updateUser(user: UserType) {
     await mongoose.connect(uri);
     try {
@@ -28,5 +15,5 @@ async function updateUser(user: UserType) {
 export async function POST(req: Request) {
     const data = await req.json();
     console.log(data);
-    registerUser(data);
+    updateUser(data);
 }
