86b391a0ec4beae05555cf5117ba894c95351d07
DraconDev <dracsharp@gmail.com>
2023-12-14 00:11:47 +0000

    style colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,8 @@ module.exports = {
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
             },
             colors: {
-                primary: "#222", // black
-                secondary: "#333", // dark gray
+                primary: "#242424", // black
+                secondary: "#383838", // dark gray
                 accent: "#247C", // medium gray
                 accent2: "#29f",
             },
