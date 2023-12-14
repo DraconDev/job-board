1be5d59597a42428fcd6ebb00778bb9f0b40e44e
DraconDev <dracsharp@gmail.com>
2023-12-14 01:22:32 +0000

    fix style

--- components/Layout/CustomButton.tsx
+++ components/Layout/CustomButton.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function CustomButton({ text, action }: Props) {
     return (
         <button
-            className="p-2 bg-accent hover:bg-accent text-white font-bold  w-full hover:bg-primary hover:text-accent bg-border-2 border-2 border-accent rounded-sm"
+            className="p-2 bg-accent text-white font-bold  w-full hover:bg-primary hover:text-accent bg-border-2 border-2 border-accent rounded-sm"
             onClick={action}
         >
             {text}
