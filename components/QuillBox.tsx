ee4fea0880379f214439a989f77daf7e3e9da44a
DraconDev <dracsharp@gmail.com>
2023-12-17 06:10:30 +0000

    style quillbox

--- components/QuillBox.tsx
+++ components/QuillBox.tsx
@@ -1,6 +1,6 @@
 "use client";
 import ReactQuill from "react-quill";
-
+import "react-quill/dist/quill.snow.css"; // or 'quill.bubble.css' for bubble theme
 export interface IAppProps {
     userText: string;
     setUserText: (text: string) => void;
@@ -8,11 +8,13 @@ export interface IAppProps {
 
 export function QuillBox({ userText, setUserText }: IAppProps) {
     return (
-        <div className="bg-white text-black p-2">
+        <div className="  w-full h-[320px] mb-10 bg-white text-black ">
             <ReactQuill
                 theme="snow"
                 value={userText}
                 onChange={setUserText}
+                // style={{ height: "285px" }}
+                className=" rounded-lg h-[277px] "
             />
         </div>
     );
