55f2b529c8544d1748aac0c702093eb331b7b9e4
DraconDev <dracsharp@gmail.com>
2023-12-17 08:58:05 +0000

    upd description

--- components/Description/Description.tsx
+++ components/Description/Description.tsx
@@ -24,8 +24,8 @@ export default function Description() {
 
     return (
         state.activeJobPost && (
-            <div className="flex bg-primary flex-col gap-1 overflow-hidden rounded  transition-transform hover:transform hover:scale-[1.005] duration-150 ">
-                <div className="flex rounded p-3 shadow-md bg-secondary w-full  flex-col min-h-[400px]  ">
+            <div className="flex bg-primary flex-col gap-1 overflow-hidden rounded  transition-transform  ">
+                <div className="flex rounded p-3 shadow-md bg-secondary w-full  flex-col min-h-[400px]  max-h-[83vh] overflow-y-auto ">
                     <h2 className="text-xl rounded font-semibold">
                         {state?.activeJobPost?.title}
                     </h2>
--- components/Quill/ShowHtml.tsx
+++ components/Quill/ShowHtml.tsx
@@ -1,8 +1,10 @@
 export function ShowHtml({ userText }: { userText: string }) {
     return (
-        <div
-            className="w-full h-auto rounded"
-            dangerouslySetInnerHTML={{ __html: userText }}
-        />
+        <div className="description">
+            <div
+                className=""
+                dangerouslySetInnerHTML={{ __html: userText }}
+            />
+        </div>
     );
 }
