2b7047defc3981ad27cf96c65b0b3f5910df9570
DraconDev <dracsharp@gmail.com>
2023-12-17 07:13:37 +0000

    upd job card

--- components/JobContainer/JobCard.tsx
+++ components/JobContainer/JobCard.tsx
@@ -1,7 +1,7 @@
+"use client";
 import { useAppState } from "@/state/state";
 import { JobType } from "@/type/types";
 import { useState } from "react";
-import { FaMoneyBillAlt } from "react-icons/fa";
 import Description from "../Description/Description";
 import MobileSlideWrapper from "../mobile/MobileSlideWrapper";
 
@@ -27,25 +27,26 @@ const JobCard = ({ job }: { job: JobType }) => {
                 </MobileSlideWrapper>
             ) : (
                 <div
-                    className="p-2 gap-1 bg-secondary shadow-md rounded-md hover:cursor-pointer hover:transform hover:scale-[1.005] duration-150 w-full"
+                    className="p-2 flex flex-col gap-[2px] bg-secondary shadow-md rounded-md hover:cursor-pointer hover:transform hover:scale-[1.005] duration-150 w-full"
                     onClick={showDescription}
                 >
                     <h2 className="text-xl font-semibold">{job.title}</h2>
                     <div className="w-full h-[2px] bg-accent"></div>
                     <div className="flex justify-between text-gray-400">
                         <p>{job.company}</p>
-                        <div className="text-gl flex gap-2 items-center">
-                            <FaMoneyBillAlt />
-                            {job.salaryMin} - {job.salaryMax}
-                        </div>
                     </div>
-                    <p className="mt-2">
+                    <div className="text-lg flex   items-center">
+                        ${job.salaryMin} - ${`${job.salaryMax}`}
+                    </div>
+                    {/* <p className="mt-2">
                         {job?.description && job.description.length > 300
                             ? job.description.slice(0, 300) + "..."
                             : job?.description}
-                    </p>
-                    <div className="mt-4 flex justify-between items-center">
-                        <span className="text-gray-400">{job.city}</span>
+                    </p> */}
+                    <div className=" flex justify-between items-center">
+                        <span className="text-gray-400">
+                            {job.date?.slice(0, 10).toString()}
+                        </span>
                     </div>
                 </div>
             )}
