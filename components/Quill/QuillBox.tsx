1ebaf1c6c2f2a5976119456500924a3252884989
DraconDev <dracsharp@gmail.com>
2023-12-17 20:45:32 +0000

    test deploy

--- components/Quill/QuillBox.tsx
+++ components/Quill/QuillBox.tsx
@@ -1,20 +1,33 @@
 "use client";
-// import "react-quill/dist/quill.snow.css"; // or 'quill.bubble.css' for bubble theme
+
+import { useEffect, useState } from "react";
+import ReactQuill from "react-quill";
+
+import "react-quill/dist/quill.snow.css";
+// or 'quill.bubble.css' for bubble theme
 export interface IAppProps {
     userText: string;
     setUserText: (text: string) => void;
 }
 
 export function QuillBox({ userText, setUserText }: IAppProps) {
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        setLoaded(true);
+    }, []);
+
     return (
-        <div className="  w-full h-[320px] mb-10 bg-white text-black ">
-            {/* <ReactQuill
-                theme="snow"
-                value={userText}
-                onChange={setUserText}
-                // style={{ height: "285px" }}
-                className=" rounded-lg h-[277px] "
-            /> */}
-        </div>
+        loaded && (
+            <div className="  w-full h-[320px] mb-10 bg-white text-black ">
+                <ReactQuill
+                    theme="snow"
+                    value={userText}
+                    onChange={setUserText}
+                    // style={{ height: "285px" }}
+                    className=" rounded-lg h-[277px] "
+                />
+            </div>
+        )
     );
 }
