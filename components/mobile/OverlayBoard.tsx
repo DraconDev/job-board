0fb25d59b0eff6cbb4c217088efa356cb78104a4
DraconDev <dracsharp@gmail.com>
2023-12-17 21:02:16 +0000

    overlay hide on clickaway fully works

--- components/mobile/OverlayBoard.tsx
+++ components/mobile/OverlayBoard.tsx
@@ -15,11 +15,11 @@ const OverlayBoard = ({ isOpen, toggleOverlayMenu }: Props) => {
             } transition-width duration-50 flex justify-center`}
         >
             {isOpen && (
-                <div
-                    className="flex justify-center mt-2 md:max-w-5xl w-full"
-                    ref={box}
-                >
-                    <div className="flex  flex-col gap-1 mt-2 w-2/3">
+                <div className="flex justify-center mt-2 md:max-w-5xl w-full">
+                    <div
+                        className="flex  flex-col gap-1 mt-2 w-2/3 h-fit"
+                        ref={box}
+                    >
                         <ListBoxes />
                         <button
                             className="p-2 bg-secondary hover:bg-accent text-white font-bold rounded w-full"
