c9c143f9262a7334e464fd39477b950c251d2bb5
DraconDev <dracsharp@gmail.com>
2023-12-18 09:41:17 +0000

    upd job filter

--- utils/filterjob.ts
+++ utils/filterjob.ts
@@ -4,12 +4,15 @@ export function useSearchJobs() {
     const router = useRouter();
     const state = useAppState((state) => state);
     async function Search() {
+        const title = state.searchTitle.trim().toLowerCase();
+        if (title.length < 3) {
+            alert("Please enter at least 3 characters");
+            return;
+        }
         const queryParams = new URLSearchParams();
 
-        console.log(state);
-
         if (state.searchTitle) {
-            queryParams.append("title", state.searchTitle.trim().toLowerCase());
+            queryParams.append("title", title);
         }
 
         if (state.searchLocation) {
@@ -47,32 +50,26 @@ export function useSearchJobs() {
             );
         }
 
-        if (state.searchTitle.length < 3) {
-        } else {
-            try {
-                const jobs = await fetch(
-                    `/api/job/find/filter?${queryParams.toString()}`,
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                    }
-                );
+        try {
+            const jobs = await fetch(
+                `/api/job/find/filter?${queryParams.toString()}`
+                // {
+                //     headers: {
+                //         "Content-Type": "application/json",
+                //     },
+                // }
+            );
 
-                const data = await jobs.json();
-                console.log(data);
-                state.setJobSearchList([...data]);
-                state.updateActiveJobPost(data[0]);
+            const data = await jobs.json();
+            console.log(data);
+            state.setJobSearchList([...data]);
+            state.updateActiveJobPost(data[0]);
 
-                await router.push(
-                    `/search/${encodeURIComponent(state.searchTitle)}`
-                );
-            } catch (error) {
-                console.error("Error fetching jobs:", error);
-            } finally {
-                // Code to run regardless of success or failure
-                // ...
-            }
+            await router.push(
+                `/search/${encodeURIComponent(state.searchTitle)}`
+            );
+        } catch (error) {
+            console.error("Error fetching jobs:", error);
         }
     }
     return Search;
