244eb40c9550ff70a1c9a3fe715868901e043d46
DraconDev <dracsharp@gmail.com>
2023-12-21 15:49:03 +0000

    add searchtype switcher

---
+++ app/tasks/page.tsx
@@ -0,0 +1,13 @@
+type Props = {};
+import { RiTaskFill } from "react-icons/ri";
+
+const Tasks = (props: Props) => {
+    return (
+        <div>
+            Tasks
+            <RiTaskFill />
+        </div>
+    );
+};
+
+export default Tasks;
--- components/Navbar/NavBar.tsx
+++ components/Navbar/NavBar.tsx
@@ -2,15 +2,18 @@
 import { useAppState } from "@/state/state";
 
 import { useSearchJobs } from "@/utils/filterjob";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { FaAngleDoubleDown } from "react-icons/fa";
 import LoginLogo from "../Auth/LoginLogo";
 import FetchJobsButton from "./FetchJobsButton";
 import LogoButton from "./LogoButton";
 import SearchBar from "./SearchBar";
 
 export default function NavBar() {
+    const searchOptions = ["jobs", "tasks"];
     const state = useAppState((state) => state);
     const searchJobs = useSearchJobs();
+    const [searchType, setSearchType] = useState<"jobs" | "tasks">("jobs");
 
     const handleKeyDown = useCallback(
         (event: any) => {
@@ -30,13 +33,23 @@ export default function NavBar() {
     }, [handleKeyDown]);
 
     return (
-        <div className="bg-secondary justify-center flex items-center ">
+        <div className="bg-secondary justify-center flex items-center h-full">
             <div className="max-w-5xl p-1 gap-1 justify-between w-full flex items-center">
                 <div className="flex gap-1 items-center">
                     <LogoButton type="home" />
                 </div>
-                <div className="flex gap-1 px-1 grow items-center">
-                    <div className="w-full">
+                <div className="flex gap-1 px-1 grow items-center h-full">
+                    <div className="w-full flex h-full ">
+                        <button
+                            className="w-10 h-10  bg-primary text-accent text-lg flex items-center justify-center  "
+                            onClick={() =>
+                                setSearchType(
+                                    searchType == "jobs" ? "tasks" : "jobs"
+                                )
+                            }
+                        >
+                            <FaAngleDoubleDown className="w-6 h-6" />
+                        </button>
                         <SearchBar
                             placeholder="Job"
                             logo="search"
