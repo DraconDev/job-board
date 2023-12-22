95330e50c3b8d8a1a89838b19fbe9217a953c0e9
DraconDev <dracsharp@gmail.com>
2023-12-22 21:23:00 +0000

    style nav

--- components/Navbar/SearchTypesDropdown.tsx
+++ components/Navbar/SearchTypesDropdown.tsx
@@ -1,25 +1,13 @@
 "use client";
 
-import react from "react";
-
 type Props = {
     searchOptions: string[];
     setSearchType: (type: "jobs" | "tasks") => void;
-
-    ref: react.RefObject<HTMLDivElement>;
 };
 
-const SearchTypesDropdown = ({
-    searchOptions,
-    setSearchType,
-
-    ref,
-}: Props) => {
+const SearchTypesDropdown = ({ searchOptions, setSearchType }: Props) => {
     return (
-        <div
-            className="w-20 absolute left-0 top-0 mt-10 bg-secondary  border-2 border-primary text-lg  "
-            ref={ref}
-        >
+        <div className="w-20 absolute left-0 top-0 mt-10 bg-secondary  border-2 border-primary text-lg  ">
             {searchOptions.map((e, i) => (
                 <button
                     className="hover:bg-accent p-2 text-center w-full"
