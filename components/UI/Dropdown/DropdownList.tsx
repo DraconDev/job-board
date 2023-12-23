fcc0ff3f02623b555f719a84300f406d119abf86
DraconDev <dracsharp@gmail.com>
2023-12-23 14:40:52 +0000

    upd dropdownmenu

--- components/UI/Dropdown/DropdownList.tsx
+++ components/UI/Dropdown/DropdownList.tsx
@@ -5,16 +5,15 @@ type Props = {
     list: string[];
     set?: (e: string) => void;
     close?: () => void;
-    
 };
 
 const DropdownMenu = ({ list, set, close }: Props) => {
     return (
-        <div className="w-20 absolute left-0 top-0 mt-[44px] bg-secondary border-2 border-primary text-lg flex flex-col">
+        <div className="w-20 absolute left-0 top-0 mt-[44px] bg-secondary border-2 border-primary text-lg flex flex-col ">
             {list.map((e, i) =>
                 set ? (
                     <button
-                        className="hover:bg-accent p-2 text-center w-full"
+                        className="hover:bg-accent p-2 py-1 text-center w-full"
                         key={i}
                         onClick={() => {
                             set(e);
@@ -27,7 +26,7 @@ const DropdownMenu = ({ list, set, close }: Props) => {
                     <Link
                         href={"/" + "post" + e}
                         key={i}
-                        className="hover:bg-accent p-2 text-center w-full"
+                        className="hover:bg-accent p-2 py-1 text-center w-full"
                     >
                         {e[0].toUpperCase() + e.slice(1)}
                     </Link>
--- components/UI/Dropdown/Dropmenu.tsx
+++ components/UI/Dropdown/Dropmenu.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+"use client";
+import React, { useRef, useState } from "react";
 import DropdownList from "./DropdownList";
 
 type Props = {
@@ -9,13 +10,35 @@ type Props = {
 
 const Dropmenu = ({ list, set, children }: Props) => {
     const [toggle, setToggle] = useState(false);
+    const closeTimeoutId = useRef<NodeJS.Timeout | null>(null);
+
+    const handleMouseEnter = () => {
+        // Clear any existing timeouts to prevent the dropdown from closing
+        if (closeTimeoutId.current) {
+            clearTimeout(closeTimeoutId.current);
+            closeTimeoutId.current = null;
+        }
+        setToggle(true);
+    };
+
+    const handleMouseLeave = () => {
+        // Set a timeout to close the dropdown after a delay
+        closeTimeoutId.current = setTimeout(() => {
+            setToggle(false);
+        }, 50); // Delay in milliseconds, adjust as needed
+    };
+
     return (
-        <div className="flex  relative  border-accent rounded-md text-white">
+        <div
+            className="flex relative border-accent rounded-md text-white"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
             <button
-                className="p-1 w-10 h-10   text-accent text-lg flex items-center justify-center  "
+                className="p-1 w-10 h-10 text-accent text-lg flex items-center justify-center hover:bg-accent rounded-md"
                 onClick={() => setToggle(!toggle)}
             >
-                {children && children}
+                {children}
             </button>
             {toggle && (
                 <DropdownList
