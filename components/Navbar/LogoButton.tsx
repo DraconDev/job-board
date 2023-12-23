e2eb4544d02dc6ddcce3f05b8f6e9456df814718
DraconDev <dracsharp@gmail.com>
2023-12-23 09:46:55 +0000

    style logo

--- components/Navbar/LogoButton.tsx
+++ components/Navbar/LogoButton.tsx
@@ -4,10 +4,12 @@ import Link from "next/link";
 export default function Logo({ type }: { type: string }) {
     return (
         <>
-            <Link href={type === "home" ? "/" : `/${type}`}>
-                {/* <Link href="work"> */}
-                <div className="h-full px-1 hover:bg-accent rounded-lg">
-                    <div className="flex items-center justify-center ">
+            <Link
+                href={type === "home" ? "/" : `/${type}`}
+                className="h-full"
+            >
+                <div className="h-full p-1 hover:bg-accent rounded-lg">
+                    <div className="flex items-center justify-center h-full ">
                         <Image
                             src={`${type}.svg`}
                             alt={type}
