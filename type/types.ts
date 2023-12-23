23066f1f53dd25f469ea46f850079742a38ef16d
DraconDev <dracsharp@gmail.com>
2023-12-23 10:34:02 +0000

    upd task type

--- type/types.ts
+++ type/types.ts
@@ -1,18 +1,9 @@
-import { jobSchema, userSchema } from "@/db/schema";
+import { jobSchema, taskSchema, userSchema } from "@/db/schema";
 import mongoose from "mongoose";
 
 export type JobType = mongoose.InferSchemaType<typeof jobSchema>;
 
 export type UserType = mongoose.InferSchemaType<typeof userSchema>;
 
-export type Task = {
-    title: string;
-    description: string;
-    status?: string;
-    dueDate?: Date;
-    postDate: Date;
-    priority?: string;
-    assignee?: string;
-    reviewer?: string;
-    bounty: number;
-};
+// task type
+export type TaskType = mongoose.InferSchemaType<typeof taskSchema>;
