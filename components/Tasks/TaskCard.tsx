6a026370709d4d3a9e2ace9748d6c7197ecbe9df
DraconDev <dracsharp@gmail.com>
2023-12-23 16:00:09 +0000

    upd task card style

--- components/Tasks/TaskCard.tsx
+++ components/Tasks/TaskCard.tsx
@@ -4,13 +4,15 @@ import { GiMoneyStack } from "react-icons/gi";
 const TaskCard = ({ task }: { task: TaskType }) => {
     return (
         <div className="flex flex-col bg-secondary p-2 min-h-[250px] min-w-[250px] grow hover:bg-opacity-60 rounded-lg max-w-[350px]">
-            <div className="flex flex-col hover:scale-[1.01] h-full">
+            <div className="flex flex-col hover:scale-[1.01] h-full grow">
                 <h2 className="text-xl items-center font-semibold text-center">
                     {task.title}
                 </h2>
                 <div className="w-full h-[2px] bg-accent my-1"></div>
-                <div className="flex flex-col justify-between h-full grows">
-                    <p className="">{task.description}</p>
+                <div className="flex flex-col justify-between  grow h-full">
+                    <p className="flex grow w-full h-full">
+                        {task.description}
+                    </p>
                     <div className="text-gl gap-2  justify-between items-center flex">
                         <div className="flex justify-between text-gray-400 gap-1 items-center w-full">
                             <div className="flex gap-1 ">
