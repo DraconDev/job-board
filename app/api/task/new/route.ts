98a0d8ad1020ab216bc3cb184c5a41788b3b5c4b
DraconDev <dracsharp@gmail.com>
2023-12-23 10:37:12 +0000

    upd post task route

--- app/api/task/new/route.ts
+++ app/api/task/new/route.ts
@@ -1,18 +1,13 @@
 // create new task
 import { uri } from "@/db/mongo";
 import { Task } from "@/db/schema";
+import { TaskType } from "@/type/types";
 import mongoose from "mongoose";
 
-async function addTask({
-    title,
-    description,
-}: {
-    title: string;
-    description: string;
-}) {
+async function addTask(info: TaskType) {
     try {
         await mongoose.connect(uri);
-        const task = await Task.create({ title, description });
+        const task = await Task.create({ ...info });
         return task;
     } finally {
         await mongoose.disconnect();
@@ -21,11 +16,11 @@ async function addTask({
 
 export async function POST(request: Request) {
     const data = await request.json();
-    const { title, description } = data;
+    const { title, description } = data as TaskType;
     if (!title || !description) {
         throw new Error("Missing title or description in the request");
     }
-    const task = await addTask({ title, description });
+    const task = await addTask({ ...data });
     return new Response(JSON.stringify(task), {
         headers: {
             "Content-Type": "application/json",
