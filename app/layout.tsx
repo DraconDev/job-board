f8d9638ffc59734d4a119a507f9a1525ffbc8816
DraconDev <dracsharp@gmail.com>
2023-12-23 09:52:33 +0000

    add react query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import AuthProvider from "@/components/Auth/AuthProvider";
 import NavBar from "@/components/Navbar/NavBar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { Metadata } from "next";
 import { Kanit } from "next/font/google";
 import "./globals.css";
@@ -16,23 +18,28 @@ export default function RootLayout({
 }: {
     children: React.ReactNode;
 }) {
+    const queryClient = new QueryClient();
+
     return (
         <html
             lang="en"
             className="    bg-primary "
         >
             <AuthProvider>
-                <body className={`${font.className} h-[100vh]  `}>
-                    <div className="flex w-full h-full flex-col ">
-                        <NavBar />
-                        {/* <ThemeSwitcher /> */}
-                        <div className="flex  justify-center h-full w-full bg-primary">
-                            <div className="flex  justify-center h-full max-w-5xl w-full  items-center flex-col pt-[52px]">
-                                {children}
+                <QueryClientProvider client={queryClient}>
+                    <body className={`${font.className} h-[100vh]  `}>
+                        <div className="flex w-full h-full flex-col ">
+                            <NavBar />
+                            {/* <ThemeSwitcher /> */}
+                            <div className="flex  justify-center h-full w-full bg-primary">
+                                <div className="flex  justify-center h-full max-w-5xl w-full  items-center flex-col pt-[52px]">
+                                    {children}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </body>
+                    </body>
+                    <ReactQueryDevtools initialIsOpen={false} />
+                </QueryClientProvider>
             </AuthProvider>
         </html>
     );
